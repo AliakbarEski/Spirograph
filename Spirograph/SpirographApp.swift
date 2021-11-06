645bdcfc284c1279f4708a837a036a60e3c9188e
AliakbarEski <eski@nanoleaf.me>
2021-11-06 16:30:58 -0400

    Initial Commit

---
+++ Spirograph/SpirographApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpirographApp.swift
+//  Spirograph
+//
+//  Created by Aliakbar Eski on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct SpirographApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
